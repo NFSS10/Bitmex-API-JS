33f2dde089af6b9dfc120428321ad50bf0ff018a
Nuno Silva <nuno_nfss96@hotmail.com>
2022-06-29 20:44:38 +0100

    API and WebSocket Improvements (#3)
    * Support API methods: `listOrdersOpen()`, `cancelOrders` and `stopMarket()`
    
    * Support WebSocket authentication
    
    * Pretty
    
    * Avoid websocket hanging event loop
    
    * Support `updateOrder()` API method
    
    * Improve `getHistoricalData()` and added `getAllHistoricalData()`
    
    * Pretty

--- src/bitmex/api.js
+++ src/bitmex/api.js
@@ -54,6 +54,58 @@ const __post = async (baseUrl, route, body, apiSecret, apiKey) => {
     return json;
 };
 
+const __put = async (baseUrl, route, body, apiSecret, apiKey) => {
+    const requestType = "PUT";
+    const routePath = `${API_PATH}${route}`;
+    const bodyStr = JSON.stringify(body);
+
+    const expires = Math.round(Date.now() / 1000) + 60; // 1 min. from now
+    const signature = crypto
+        .createHmac("sha256", apiSecret)
+        .update(requestType + routePath + expires + bodyStr)
+        .digest("hex");
+
+    const url = `${baseUrl}${routePath}`;
+    const response = await fetch(url, {
+        method: requestType,
+        body: bodyStr,
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": apiKey,
+            "api-expires": expires,
+            "api-signature": signature
+        }
+    });
+    const json = await response.json();
+    return json;
+};
+
+const __delete = async (baseUrl, route, body, apiSecret, apiKey) => {
+    const requestType = "DELETE";
+    const routePath = `${API_PATH}${route}`;
+    const bodyStr = JSON.stringify(body);
+
+    const expires = Math.round(Date.now() / 1000) + 60; // 1 min. from now
+    const signature = crypto
+        .createHmac("sha256", apiSecret)
+        .update(requestType + routePath + expires + bodyStr)
+        .digest("hex");
+
+    const url = `${baseUrl}${routePath}`;
+    const response = await fetch(url, {
+        method: requestType,
+        body: bodyStr,
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": apiKey,
+            "api-expires": expires,
+            "api-signature": signature
+        }
+    });
+    const json = await response.json();
+    return json;
+};
+
 const __sleep = async ms => {
     return new Promise(resolve => setTimeout(resolve, ms));
 };
@@ -79,6 +131,30 @@ class API {
         const response = await fetch(url);
         const json = await response.json();
         return json;
+    }
+
+    async updateOrder(orderId, body = {}) {
+        body.orderID = orderId;
+        const result = await __put(this._baseUrl, "/order", body, this._apiSecret, this._apiKey);
+        return result;
+    }
+
+    async listOrdersOpen(symbol) {
+        const params = {
+            symbol: symbol,
+            filter: JSON.stringify({ open: true })
+        };
+        const result = await __get(this._baseUrl, "/order", params, this._apiSecret, this._apiKey);
+        return result;
+    }
+
+    async cancelOrders(ids, clOrdID = null) {
+        const body = {
+            orderID: ids,
+            clOrdID: clOrdID
+        };
+        const result = await __delete(this._baseUrl, "/order", body, this._apiSecret, this._apiKey);
+        return result;
     }
 
     async buyMarket(symbol, orderQty, clOrdID = null) {
@@ -147,6 +223,19 @@ class API {
         return result;
     }
 
+    async stopMarket(symbol, side, orderQty, price, clOrdID = null) {
+        const body = {
+            ordType: "Stop",
+            side: side,
+            symbol: symbol,
+            orderQty: orderQty,
+            stopPx: price,
+            clOrdID: clOrdID
+        };
+        const result = await __post(this._baseUrl, "/order", body, this._apiSecret, this._apiKey);
+        return result;
+    }
+
     async setLeverage(symbol, leverage = 1) {
         const body = {
             symbol: symbol,
@@ -163,28 +252,37 @@ class API {
         return result;
     }
 
-    async getHistoricalData(symbol, interval = "1m", startTime = null, endTime = null) {
-        const result = [];
+    async getHistoricalData(symbol, interval = "1m", start = 0, count = 1000, startTime = null, endTime = null) {
+        if (count > 1000) throw new Error("'count' cannot be greater than 1000");
         const params = {
-            count: 1000,
-            start: 0,
+            count: count,
+            start: start,
             symbol: symbol,
             binSize: interval,
             startTime: startTime,
             endTime: endTime
         };
+        const result = await __get(this._baseUrl, "/trade/bucketed", params, this._apiSecret, this._apiKey);
+        return result;
+    }
 
+    async getAllHistoricalData(symbol, interval = "1m", startTime = null, endTime = null) {
+        const result = [];
+
+        let start = 0;
+        const count = 1000;
         let isFinished = false;
         while (!isFinished) {
             const promises = [];
 
             // the Bitmex request limit is 120 requests per minute, so it
-            // tries avoid that limit by doing 115 requests per minute
+            // tries avoid that limit by first waiting a minute and then
+            // keep doing 115 requests per minute util it has all the data
             await __sleep(60000);
             for (let i = 0; i < 115; i++) {
-                const promise = __get(this._baseUrl, "/trade/bucketed", params, this._apiSecret, this._apiKey);
+                const promise = this.getHistoricalData(symbol, interval, start, count, startTime, endTime);
                 promises.push(promise);
-                params.start = params.start + 1000;
+                start += 1000;
             }
             const data = await Promise.all(promises);
 
